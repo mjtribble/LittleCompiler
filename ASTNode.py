1a0f14b2cb84442facd36fe97b3012c734f429e1
Melody <mjtribble2@gmail.com>
2018-04-27 10:26:43 -0600

    Cleaned up ASTNode class

--- ASTNode.py
+++ ASTNode.py
@@ -32,15 +32,6 @@ class ASTNode:
         str1 = ''.join(str(e) for e in self.value)
         return 'NODE: %s %s [ %s ] ' % (self.node_type, self.val_type, str1)
 
-    def pprint(self):
-        if self.node_type == node_enum(5).name or self.node_type == node_enum(13).name or self.node_type == node_enum(14).name or self.node_type == node_enum(15).name:
-            print('; Node: TYPE: %s VALUEs: ' % (self.node_type))
-            for item in self.value:
-                item.pprint()
-        else:
-            print('; Node: TYPE: %s VALUE: %s %s' % (self.node_type, self.val_type, self.value))
-        # print('Code Object: %s' % self.code_object)
-
     def setRightChild(self, child):
         self.rightChild = child
 
@@ -52,47 +43,3 @@ class ASTNode:
 
     def getLeftChild(self):
         return self.leftChild
-
-#  This is a special node that holds all assignment statment ast trees.
-#  It is of type STMTLST
-class AssignmentStmtList:
-
-    def __init__(self ):
-        self.node_type = node_enum(5).name
-        self.node_list = []
-        self.add("PLACEHOLDER", ASTNode(node_enum(6).name, ""))
-
-    # variable is the left hand side of the statment,
-    # will be used to find a variables that may need in larger expressions.
-    def add(self, var, node):
-        # print("Added " + var + " to the statement list")
-        self.node_list.append((var, node))
-
-    def remove(self):
-        self.node_list.pop()
-
-    def assPrint(self):
-        print("Available Assignment Variables:")
-        for node in self.node_list:
-            print("var = " + node[0])
-
-    # This will find a variable and return the assignment tree root node or None if it doesn't exist
-    def findVariable(self, var):
-        # this returns the tuple in list form [(var, node)] if found or an empty list if not.
-        stmt = [item for item in self.node_list if item[0] == var]
-        if stmt:
-            # this pulls the node out of the list->tuple obj
-            node = stmt[0][1]
-            # self.assPrint()
-            # print(stmt[0][0] +" is being removed from the statement list")
-            # print(stmt[0])
-
-            # this removes the smt from the list since we've already used it
-            self.node_list.remove(stmt[0])
-            # self.assPrint()
-            # print("Find Variable is returning: ")
-            # print(node)
-            return node
-        else:
-            # print("Find Variable is returning None ")
-            return None
